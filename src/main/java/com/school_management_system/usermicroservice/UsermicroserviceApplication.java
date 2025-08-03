2d73a58659c446e3b0c0f45b6e4780cfa7fbcd86
Harsh Choudhary <harsh.choudhary@unthinkable.co>
2025-08-03 10:35:37 +0530

    initial configuration

--- src/main/java/com/school_management_system/usermicroservice/UsermicroserviceApplication.java
+++ src/main/java/com/school_management_system/usermicroservice/UsermicroserviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchoolManagementSystemUsermicroserviceApplication {
+public class UsermicroserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchoolManagementSystemUsermicroserviceApplication.class, args);
+		SpringApplication.run(UsermicroserviceApplication.class, args);
 	}
 
 }
--- src/test/java/com/school_management_system/usermicroservice/UsermicroserviceApplicationTests.java
+++ src/test/java/com/school_management_system/usermicroservice/UsermicroserviceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SchoolManagementSystemUsermicroserviceApplicationTests {
+class UsermicroserviceApplicationTests {
 
 	@Test
 	void contextLoads() {
